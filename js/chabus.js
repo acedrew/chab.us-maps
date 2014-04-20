6e74e2596024f3f7da7250b33c0bd13d1216ddbb
Andrew Rodgers <acedrew@gmail.com>
2014-04-20 19:53:28 +0000

    numerous improvements, refactoring, added initial bus positions

--- js/chabus.js
+++ js/chabus.js
@@ -1,6 +1,6 @@
-
-function chabusInitialize(map) {
-    var directions = {
+function drawbus(bus) {
+    var buses = {};
+    var headings = {
         N: 0,
         NNE: 22.5,
         NE: 45,
@@ -17,7 +17,39 @@ function chabusInitialize(map) {
         WNW: 292.5,
         NW: 315,
         NNW: 337.5
-    };
+    }
+        buslocation =  new google.maps.LatLng(parseFloat(bus.lat), parseFloat(bus.lon));
+        var heading = headings[bus.heading];
+        if(!buses[bus.id]) {
+            buses[bus.id] = new google.maps.Marker({
+                position: buslocation,
+                title: ("Bus #" + bus.id),
+                icon: {
+                    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                    scale: 3,
+                    rotation: heading
+                },
+                map: map
+                });
+        } else {
+            buses[bus.id].setPosition(buslocation);
+            var iconUpdate = buses[bus.id].getIcon();
+            iconUpdate.rotation = heading;
+            buses[bus.id].setIcon(iconUpdate);
+        }
+    }
+    
+function chabusInitialize(map) {
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "http://api.chab.us/buses", false);
+    xmlHttp.send();
+    var initialbuses = JSON.parse(xmlHttp.responseText);
+    console.log(initialbuses);
+    for(var i = 0; i < initialbuses.length; i++) {
+        console.log(initialbuses[i]);
+        var newBus = initialbuses[i];
+        drawbus(newBus);
+    }
     markerlocation = new google.maps.LatLng(35.0344, -85.2700);
     //var marker = new google.maps.Marker({
     //    position: markerlocation,
@@ -28,32 +60,10 @@ function chabusInitialize(map) {
     //        },
     //    map: map
     //    });
-    window.map = map;
-    var buses = {};
     new EventSource('http://api.chab.us/buses/tail').addEventListener('change', function (x) { 
             var json = JSON.parse(x.data);
             var bus = json.bus;
-            console.log(bus.id);
-            buslocation =  new google.maps.LatLng(parseFloat(bus.lat), parseFloat(bus.lon));
-            var heading = directions[bus.heading];
-            if(!buses[bus.id]) {
-                buses[bus.id] = new google.maps.Marker({
-                    position: buslocation,
-                    title: ("Bus #" + bus.id),
-                    icon: {
-                        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-                        scale: 3,
-                        rotation: heading
-                    },
-                    map: map
-                    });
-            } else {
-                buses[bus.id].setPosition(buslocation);
-                var iconUpdate = buses[bus.id].getIcon();
-                iconUpdate.rotation = heading;
-                buses[bus.id].setIcon(iconUpdate);
-            }
-                 
-                
+            //console.log(bus.id);
+            drawbus(bus);
              });
 }
